67a0bafca593418c9d6e2c98b047e847cc0304bd
himanshugoyal2000 <f20180753@hyderabad.bits-pilani.ac.in>
2019-06-10 10:45:20 +0530

    Updates

---
+++ precache-manifest.427982bf988ed5720755e53f85005aad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebd2cfdc12964530087af633e41ce18",
+    "url": "/smartcampus_project/index.html"
+  },
+  {
+    "revision": "addae662e0939a3b03e7",
+    "url": "/smartcampus_project/static/css/main.e739769c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/smartcampus_project/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "addae662e0939a3b03e7",
+    "url": "/smartcampus_project/static/js/main.5b6bae82.chunk.js"
+  },
+  {
+    "revision": "2c4a254c22e8cd03729c",
+    "url": "/smartcampus_project/static/js/runtime~main.9561a2a6.js"
+  },
+  {
+    "revision": "81bfa4e1f564453fb8e33e3cbdddb2d3",
+    "url": "/smartcampus_project/static/media/pic.81bfa4e1.jpg"
+  }
+]);
